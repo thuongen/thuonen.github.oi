ddd8e832cd7efae5ac7fd13fa497e46f93a9a38c
thuongen <enthuong333@gmail.com>
2025-01-10 12:56:16 +0700

    khaibaobien

---
+++ tet1.java
@@ -0,0 +1,9 @@
+package JAVA_tet;
+
+public class tet1 {
+    public static void main(String[] args) {
+        int  a =10*2+16/4;//24
+        System.out.println("a="+a);
+        System.out.println("thuong en an tet đay");
+    }
+}
